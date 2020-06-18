1776caa15f7f7fece9c34e2455b61e76ed507607
katrin-kot <enkor89@mail.ru>
2020-06-18 13:03:06 +0300

    fix: delete escape symbols

--- src/pages/login/validate.js
+++ src/pages/login/validate.js
@@ -7,8 +7,7 @@ export function validate(input) {
       input.value
         .trim()
         .match(
-          // eslint-disable-next-line no-useless-escape
-          /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/,
+          /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9_\-.]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/,
         ) == null
     ) {
       return false;
@@ -22,7 +21,6 @@ export function validate(input) {
       !input.value
         .trim()
         .match(
-          // eslint-disable-next-line no-useless-escape
           /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[+\-_@$!%*?&#.,;:[\]{}])[0-9a-zA-Z+\-_@$!%*?&#.,;:[\]{}]{8,}/g,
         )
     ) {
