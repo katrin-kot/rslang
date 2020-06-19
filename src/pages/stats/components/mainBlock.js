4ca9b7287dc7c0825a95204211b9e7e29094b4e8
Stanislav Vitvitskiy <stanislavvitvitskiy@Stanislavs-MacBook-Pro.local>
2020-06-19 16:22:49 -0700

    feat: add bar chart

--- src/pages/stats/components/mainBlock.js
+++ src/pages/stats/components/mainBlock.js
@@ -9,7 +9,11 @@ export function mainBlock() {
     courseCompletion.className = 'course-completion'
     const completionChart = document.createElement('div');
     completionChart.id = 'chart';
-
+    const barChartElem = document.createElement('div');
+    barChartElem.id = 'bar-chart';
+    const pieChartBlock = document.createElement('div');
+    pieChartBlock.className = 'pie-chart-block';
+    // Course Completion Chart
     document.addEventListener('DOMContentLoaded', function(e) {
         let percent = .9;
         let text = (percent * 100) + "%";
@@ -66,8 +70,75 @@ export function mainBlock() {
             .attr('dy', '.35em')
             .text(text);
 
+
+
+        //Bar Chart
+
+        const barChart = (selector, data) => {
+            const barHeight = 25;
+            const width = 500;
+            const height = data.length * (barHeight * 2);
+            const margin = { top: 0, right: 25, bottom: 1, left: 60 };
+            const container = d3.select(selector);
+
+            const chart = container.append('svg')
+                .style('width', '100%')
+                .attr('viewBox', `0 0 ${width} ${height}`);
+            const xScale = d3.scaleLinear()
+                .range([0, width/1.2 - margin.left - margin.right])
+                .domain([0, d3.max(data, d => d.value)]);
+
+            const yScale = d3.scaleBand()
+                .range([0, height - margin.top - margin.bottom])
+                .domain(data.map(d => d.name));
+
+            const color = d3.scaleOrdinal()
+                .range(d3.schemeCategory10)
+                .domain(data.map(d => d.name));
+
+            const yAxis = chart.append('g')
+                .call(d3.axisLeft(yScale))
+                .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+            chart.selectAll('.bar')
+                .data(data)
+                .enter()
+                .append('rect')
+                .attr('class', 'bar')
+                .attr('fill', d => color(d.name))
+                .attr('height', barHeight)
+                .attr('width', d => xScale(d.value))
+                .attr('x', margin.left + 1)
+                .attr('y', d => yScale(d.name) + (barHeight / 2));
+
+            chart.selectAll('.label')
+                .data(data)
+                .enter()
+                .append('text')
+                .attr('class', 'label')
+                .attr('alignment-baseline', 'middle')
+                .attr('x', d => xScale(d.value) + margin.left + 5)
+                .attr('y', d => yScale(d.name) + (barHeight))
+                .style('font-size', '12px')
+                .style('font-weight', 'bold')
+                .text(d => d.value);
+
+            yAxis.selectAll('text').style('font-size', '12px');
+        };
+
+        const data = [
+            { name: "Кол-во пройденных карточек", value: 33 },
+            { name: "% Правильных ответов", value: 87 },
+            { name: "Кол-во новых слов", value: 12 },
+            { name: "Серия правильных ответов", value : 17},
+        ];
+
+        barChart("#bar-chart", data);
     })
-    mainContainer.appendChild(courseCompletion);
-    mainContainer.appendChild(completionChart);
+
+    pieChartBlock.appendChild(courseCompletion);
+    pieChartBlock.appendChild(completionChart);
+    mainContainer.appendChild(pieChartBlock);
+    mainContainer.appendChild(barChartElem);
     return mainContainer;
 }
