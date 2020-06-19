c409aadb0afeb5280a5977a688afb8211a084ccc
Stanislav Vitvitskiy <stanislavvitvitskiy@Stanislavs-MacBook-Pro.local>
2020-06-19 14:29:05 -0700

    feat: add title

--- src/pages/stats/components/mainBlock.js
+++ src/pages/stats/components/mainBlock.js
@@ -5,41 +5,42 @@ export function mainBlock() {
     const mainContainer = document.createElement('div');
     mainContainer.className = 'main-container';
     const courseCompletion = document.createElement('h3');
-
+    courseCompletion.innerText = 'Course Completion';
+    courseCompletion.className = 'course-completion'
     const completionChart = document.createElement('div');
     completionChart.id = 'chart';
 
     document.addEventListener('DOMContentLoaded', function(e) {
-        var percent = .9;
-        var text = (percent * 100) + "%";
+        let percent = .9;
+        let text = (percent * 100) + "%";
 
-        var width = 260;
-        var height = 260;
-        var thickness = 30;
-        var duration = 750;
-        var foregroundColor = "#0a8";
-        var backgroundColor = "#ccc";
+        let width = 260;
+        let height = 260;
+        let thickness = 30;
+        let duration = 1000;
+        let foregroundColor = "#0a8";
+        let backgroundColor = "#ccc";
 
-        var radius = Math.min(width, height) / 2;
-        var color = d3.scaleOrdinal([foregroundColor, backgroundColor]);
+        let radius = Math.min(width, height) / 2;
+        let color = d3.scaleOrdinal([foregroundColor, backgroundColor]);
 
-        var svg = d3.select("#chart")
+        let svg = d3.select("#chart")
             .append('svg')
             .attr('class', 'pie')
             .attr('width', width)
             .attr('height', height);
 
-        var g = svg.append('g')
+        let g = svg.append('g')
             .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')');
 
-        var arc = d3.arc()
+        let arc = d3.arc()
             .innerRadius(radius - thickness)
             .outerRadius(radius);
 
-        var pie = d3.pie()
+        let pie = d3.pie()
             .sort(null);
 
-        var path = g.selectAll('path')
+        let path = g.selectAll('path')
             .data(pie([0, 1]))
             .enter()
             .append('path')
@@ -53,7 +54,7 @@ export function mainBlock() {
         path.data(pie([percent, 1-percent])).transition()
             .duration(duration)
             .attrTween('d', function(d) {
-                var interpolate = d3.interpolate(this._current, d);
+                let interpolate = d3.interpolate(this._current, d);
                 this._current = interpolate(0);
                 return function(t) {
                     return arc(interpolate(t));
@@ -66,8 +67,7 @@ export function mainBlock() {
             .text(text);
 
     })
-
+    mainContainer.appendChild(courseCompletion);
     mainContainer.appendChild(completionChart);
-    console.log(1);
     return mainContainer;
 }
