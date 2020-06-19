5e8abc956976c72fb699b5e7a7deabb36a00509f
Vladimir Shikhov <featurist@yandex.ru>
2020-06-20 00:09:31 +0400

    feat: create start page

--- src/pages/sprint/sprint.js
+++ src/pages/sprint/sprint.js
@@ -1,4 +1,5 @@
 import difficultOptions from '../../components/main/difficultOptions/difficultOptions';
+import './sprint.css';
 //import './reset.css';
 class Result {
   constructor() {}
@@ -9,7 +10,23 @@ class Game {
 }
 
 class StartPage {
-  constructor() {}
+  constructor() {
+    this.additionOption = [
+      {
+        id: 'studied-words',
+        value: 'studied',
+        text: ' только изученные слова',
+        hint:
+          'В задании будут использоваться ранее изученные слова. При их отсутствии или нехватке, вам будут добавлены новые слова того же уровня сложности.',
+      },
+      {
+        id: 'use-cartoon',
+        value: 'cartoon',
+        text: ' упрощённый с изображениями',
+        hint: 'В задании будут использоваться изображения английских слов.',
+      },
+    ];
+  }
 
   getPage() {
     const body = document.querySelector('body');
@@ -22,43 +39,38 @@ class StartPage {
     gameField.insertAdjacentHTML('afterbegin', difficultOptions());
     gameField.insertAdjacentHTML(
       'beforeEnd',
+      this.getOptionField(this.additionOption)
+    );
+    gameField.insertAdjacentHTML(
+      'beforeEnd',
       this.getButton('start-button', 'START')
     );
 
-    //this.addDifficultOptions(gameField);
-    //this.addStartButton(gameField);
-  }
-
-  getDifficultOptions() {
-    const form = `
-    <form class="difficult-form" action="difficult">
-    <p><b>Выберите уровень сложности слов:</b></p>
-     <input id="radio-difficult-1" name="difficult" type="radio" value="0" checked>
-     <label class="label-difficult" for="radio-difficult-1">Базовый</label>
-     <br>
-     <input id="radio-difficult-2" name="difficult" type="radio" value="1">
-     <label class="label-difficult" for="radio-difficult-2">Лёгкий</label>
-     <br>
-     <input id="radio-difficult-3" name="difficult" type="radio" value="3">
-     <label class="label-difficult" for="radio-difficult-3">Простой</label>
-     <br>
-     <input id="radio-difficult-4" name="difficult" type="radio" value="4">
-     <label class="label-difficult" for="radio-difficult-4">Средний</label>
-     <br>
-     <input id="radio-difficult-5" name="difficult" type="radio" value="5">
-     <label class="label-difficult" for="radio-difficult-5">Сложный</label>
-     <br>
-     <input id="radio-difficult-6" name="difficult" type="radio" value="6">
-     <label class="label-difficult" for="radio-difficult-6">Максимальный</label>
-   </form> `;
-
-    return form;
+    console.log(this.getOptionField(this.additionOption));
   }
 
   getButton(buttonClass, buttonText) {
     const button = `<button class="${buttonClass}">${buttonText}</button>`;
 
     return button;
+  }
+
+  getOptionField(description) {
+    const additionOptions = description.reduce((hypertext, option) => {
+      return (
+        hypertext +
+        `
+      <div class="additonal-option">
+        <input id="${option.id}" type="checkbox" name="check" value="${option.value}">
+        <label for="${option.id}">${option.text}</label>
+        <div class="additonal-option-hint" data-title="${option.hint}">
+        </div>
+      </div>
+      `
+      );
+    }, '');
+
+    return additionOptions;
   }
 }
 
