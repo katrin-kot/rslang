f4eaccb429f973696e770b8e43e6d7556927dbde
Volkau Aliaksey <bobrui4anin@gmail.com>
2020-06-22 11:15:24 +0300

    refactor: remove prettier autoformat

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const {
-  CleanWebpackPlugin,
-} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const ENV = process.env.npm_lifecycle_event;
 const isDev = ENV === 'start';
@@ -38,129 +36,131 @@ const config = {
   mode: setDMode(),
   devtool: setDevTool(),
   module: {
-    rules: [{
-      test: /\.html$/,
-      use: [{
-        loader: 'html-loader',
-        options: {
-          minimize: false,
-        },
-      }],
-    },
-    {
-      test: /\.js$/,
-      exclude: [/node_modules/],
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                useBuiltIns: 'usage',
-                corejs: 3,
-              },
-            ],
-          ],
-        },
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: false,
+            },
+          },
+        ],
       },
-    },
-    {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
           options: {
-            sourceMap: true,
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: 3,
+                },
+              ],
+            ],
           },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            config: {
-              path: './postcss.config.js',
-            },
-          },
-        },
-      ],
-    },
-    {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            config: {
-              path: './postcss.config.js',
-            },
-          },
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
-      ],
-    },
-    {
-      test: /\.(jpe?g|png|svg|gif)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          outputPath: 'img',
-          name: '[name].[ext]',
         },
       },
       {
-        loader: 'image-webpack-loader',
-        options: {
-          bypassOnDebug: true,
-          mozjpeg: {
-            progressive: true,
-            quality: 75,
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
           },
-          // optipng.enabled: false will disable optipng
-          optipng: {
-            enabled: false,
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: { path: './postcss.config.js' },
+            },
           },
-          pngquant: {
-            quality: [0.65, 0.9],
-            speed: 4,
-          },
-          gifsicle: {
-            interlaced: false,
-            optimizationLevel: 1,
-          },
-          // the webp option will enable WEBP
-          webp: {
-            quality: 75,
-          },
-        },
+        ],
       },
-      ],
-    },
-    {
-      test: /\.(woff|woff2|ttf|otf|eot)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          outputPath: 'fonts',
-        },
-      }],
-    },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: { path: './postcss.config.js' },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img',
+              name: '[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              mozjpeg: {
+                progressive: true,
+                quality: 75,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+                optimizationLevel: 1,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
     ],
   },
 
@@ -188,10 +188,12 @@ const config = {
       chunks: ['speakIt'],
       filename: './speakIt.html',
     }),
-    new CopyWebpackPlugin([{
-      from: path.join(__dirname, 'assets'),
-      to: path.join(__dirname, 'dist', 'assets'),
-    }]),
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, 'assets'),
+        to: path.join(__dirname, 'dist', 'assets'),
+      },
+    ]),
   ],
 
   devServer: {
