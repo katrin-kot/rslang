0bf7ed77219afd99ee50b0cd71005e04de22ffc7
Stanislav Vitvitskiy <stanislavvitvitskiy@Stanislavs-MacBook-Pro.local>
2020-06-24 15:18:29 -0700

    feat: add chart.js graph

--- src/pages/stats/components/mainBlock.js
+++ src/pages/stats/components/mainBlock.js
@@ -13,37 +13,30 @@ export function mainBlock() {
     barChartElem.id = 'bar-chart';
     const pieChartBlock = document.createElement('div');
     pieChartBlock.className = 'pie-chart-block';
-    // Course Completion Chart
     document.addEventListener('DOMContentLoaded', function(e) {
+        // Course Completion Chart
         let percent = .9;
         let text = (percent * 100) + "%";
-
         let width = 260;
         let height = 260;
         let thickness = 30;
         let duration = 1000;
         let foregroundColor = "#0a8";
         let backgroundColor = "#ccc";
-
         let radius = Math.min(width, height) / 2;
         let color = d3.scaleOrdinal([foregroundColor, backgroundColor]);
-
         let svg = d3.select("#chart")
             .append('svg')
             .attr('class', 'pie')
             .attr('width', width)
             .attr('height', height);
-
         let g = svg.append('g')
             .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')');
-
         let arc = d3.arc()
             .innerRadius(radius - thickness)
             .outerRadius(radius);
-
         let pie = d3.pie()
             .sort(null);
-
         let path = g.selectAll('path')
             .data(pie([0, 1]))
             .enter()
@@ -53,8 +46,6 @@ export function mainBlock() {
                 return color(i);
             })
             .each(function(d) { this._current = d; });
-
-
         path.data(pie([percent, 1-percent])).transition()
             .duration(duration)
             .attrTween('d', function(d) {
@@ -64,42 +55,33 @@ export function mainBlock() {
                     return arc(interpolate(t));
                 }
             });
-
         g.append('text')
             .attr('text-anchor', 'middle')
             .attr('dy', '.35em')
             .text(text);
 
-
-
         //Bar Chart
-
         const barChart = (selector, data) => {
             const barHeight = 25;
             const width = 500;
             const height = data.length * (barHeight * 2);
             const margin = { top: 0, right: 25, bottom: 1, left: 60 };
             const container = d3.select(selector);
-
             const chart = container.append('svg')
                 .style('width', '100%')
                 .attr('viewBox', `0 0 ${width} ${height}`);
             const xScale = d3.scaleLinear()
                 .range([0, width/1.2 - margin.left - margin.right])
                 .domain([0, d3.max(data, d => d.value)]);
-
             const yScale = d3.scaleBand()
                 .range([0, height - margin.top - margin.bottom])
                 .domain(data.map(d => d.name));
-
             const color = d3.scaleOrdinal()
                 .range(d3.schemeCategory10)
                 .domain(data.map(d => d.name));
-
             const yAxis = chart.append('g')
                 .call(d3.axisLeft(yScale))
                 .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
             chart.selectAll('.bar')
                 .data(data)
                 .enter()
@@ -110,7 +92,6 @@ export function mainBlock() {
                 .attr('width', d => xScale(d.value))
                 .attr('x', margin.left + 1)
                 .attr('y', d => yScale(d.name) + (barHeight / 2));
-
             chart.selectAll('.label')
                 .data(data)
                 .enter()
@@ -122,20 +103,16 @@ export function mainBlock() {
                 .style('font-size', '12px')
                 .style('font-weight', 'bold')
                 .text(d => d.value);
-
             yAxis.selectAll('text').style('font-size', '12px');
         };
-
         const data = [
             { name: "Кол-во пройденных карточек", value: 33 },
             { name: "% Правильных ответов", value: 87 },
             { name: "Кол-во новых слов", value: 12 },
             { name: "Серия правильных ответов", value : 17},
         ];
-
         barChart("#bar-chart", data);
     })
-
     pieChartBlock.appendChild(courseCompletion);
     pieChartBlock.appendChild(completionChart);
     mainContainer.appendChild(pieChartBlock);
--- src/pages/stats/stats.js
+++ src/pages/stats/stats.js
@@ -2,7 +2,8 @@ import './stats.css'
 import {layoutGrid} from './components/layout'
 import {header} from './components/header'
 import {mainBlock} from "./components/mainBlock";
+import {overallStats} from "./components/overallStats";
 
 const body = document.querySelector('body');
-body.appendChild(layoutGrid([header(),mainBlock()]));
+body.appendChild(layoutGrid([header(),mainBlock(),overallStats()]));
 
