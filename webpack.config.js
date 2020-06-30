f62d3e91b247583b140793039b849ec581817480
ulmas <rakhmatullaevulmas@gmail.com>
2020-06-30 14:04:45 +0500

    refractor: changes to code style

--- webpack.config.js
+++ webpack.config.js
@@ -173,19 +173,19 @@ const config = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
-    // new HtmlWebPackPlugin({
-    //   title: 'RS lang-main',
-    //   favicon: './assets/images/favicon.ico',
-    //   chunks: ['main'],
-    //   filename: './main.html',
-    // }),
-    // new HtmlWebPackPlugin({
-    //   title: 'RS lang',
-    //   favicon: './assets/images/favicon.ico',
-    //   chunks: ['login'],
-    //   template: 'src/pages/login/login.html',
-    //   filename: './index.html',
-    // }),
+    new HtmlWebPackPlugin({
+      title: 'RS lang-main',
+      favicon: './assets/images/favicon.ico',
+      chunks: ['main'],
+      filename: './main.html',
+    }),
+    new HtmlWebPackPlugin({
+      title: 'RS lang',
+      favicon: './assets/images/favicon.ico',
+      chunks: ['login'],
+      template: 'src/pages/login/login.html',
+      filename: './index.html',
+    }),
 
     new HtmlWebPackPlugin({
       //title: 'RS lang-audioCall',
@@ -238,7 +238,7 @@ const config = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 3002,
+    port: 3000,
     overlay: true,
     stats: 'errors-only',
     clientLogLevel: 'none',
