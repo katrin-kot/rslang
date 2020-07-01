4cbbed8da811ac72904fdb81909e38193ce8d9d0
Vladimir Shikhov <featurist@yandex.ru>
2020-07-01 22:46:01 +0400

    feat: add error catching

--- src/services/verifyUserService.js
+++ src/services/verifyUserService.js
@@ -2,11 +2,16 @@ import { errorWindow } from '../components/main/errorWindow/errorWindow';
 import { getAllUserWords } from './userWordService';
 import { getUserID, getToken } from './authService';
 
-export const checkUserLogin = () => {
+export const checkUserLogin = async () => {
   const userId = getUserID();
   const token = getToken();
 
-  if (userId && token && getAllUserWords({ userId })) {
+  if (userId && token) {
+    try {
+      await getAllUserWords({ userId });
+    } catch (error) {
+      errorWindow();
+    }
   } else {
     errorWindow();
   }
