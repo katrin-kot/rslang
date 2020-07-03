151641caedd68ca96e20d608b2f672f67d664619
Vladimir Shikhov <featurist@yandex.ru>
2020-07-03 12:56:11 +0400

    fix: reset game data

--- src/pages/sprint/game.js
+++ src/pages/sprint/game.js
@@ -90,8 +90,49 @@ export default class Game extends GameWindow {
     this.startPage = startPage;
     this.gamePage = gamePage;
     this.resultPage = resultPage;
-
+    this.clearStatistic();
     this.getPage();
+  }
+
+  clearStatistic() {
+    this.correctQueue = 0;
+    this.words.score = 0;
+    this.words.recievedWords = [];
+    this.words.ingameWords = [];
+    this.words.correctWords = [];
+    this.words.wrongWords = [];
+    this.words.usePages = [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+      29,
+    ];
   }
 
   getPage() {
