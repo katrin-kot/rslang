e9e95caaa17e29e4921c656bcc9f16a5ba219bb6
Julia Zaripova <zaripovaur@gmail.com>
2020-07-06 17:43:13 +0400

    feat: add service for game

---
+++ src/services/SRgameWordsService.js
@@ -0,0 +1,47 @@
+import { getToken, getUserID } from './authService';
+
+const token = getToken();
+const userID = getUserID();
+
+const getAllUserAggregatedWords = async (filter, wordsPerPage) => {
+  const rawResponse = await fetch(
+    `https://afternoon-falls-25894.herokuapp.com/users/${userID}/aggregatedWords?wordsPerPage=${wordsPerPage}&filter=${filter}`,
+    {
+      method: 'GET',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      },
+    },
+  );
+  const content = await rawResponse.json();
+  return content;
+};
+
+
+export const getNewWords = async () => {
+  const filterForNewWords = '{"userWord":null}';
+  const response = await getAllUserAggregatedWords(filterForNewWords, 5);
+  return response[0].paginatedResults;
+};
+
+export const getLearningWords = async () => {
+  const filterForLearningWords = '{"userWord":{"$ne":null}}';
+  const response = await getAllUserAggregatedWords(filterForLearningWords, 3600);
+  return response[0].paginatedResults;
+};
+
+export const filterLearningWordsPerDate = async () => {
+  const filteredWords = [];
+  const today = new Date();
+  const allWords = await getLearningWords();
+  allWords.forEach((word) => {
+    const dateToShow = new Date(word.userWord.optional.dateToShow);
+    const { status } = word.userWord.optional;
+    if (today > dateToShow && status === 'to_study') {
+      filteredWords.push(word);
+    }
+  });
+  return filteredWords;
+};
